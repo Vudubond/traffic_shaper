f2fa1a94f71665855b57ff81947b7802250bf44b
Vu3u <vudubond@gmail.com>
2024-01-11 14:57:55 +0200

    Update traffic-control.sh

--- traffic-control.sh
+++ traffic-control.sh
@@ -9,8 +9,8 @@ VIRTUAL="ifb0"
 DIRECTION=3
 # Speed
 # Speed
-SPEED_DOWNLOAD="10mbit"
-SPEED_UPLOAD="10mbit"
+SPEED_DOWNLOAD="100mbit"
+SPEED_UPLOAD="100mbit"
 
 #bit: Bits per second (bps)
 #kbit: Kilobits per second (kbps)
